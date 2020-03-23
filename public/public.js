635ca5a4d0a0edfc688cedf7ab89f24a238b412a
Markus <marxusnieminen@gmail.com>
2020-03-23 13:48:23 +0200

    Update public.js

--- public/public.js
+++ public/public.js
@@ -11,6 +11,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${pport}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 
 });
